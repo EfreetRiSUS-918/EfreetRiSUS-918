5925499361cb0bb3e7f175384f2fdb27942b960b
EfreetRiSUS-918 <116418905+EfreetRiSUS-918@users.noreply.github.com>
2022-10-28 17:36:37 +0700

    Simple 5 seconds countdown.py
    Just a simple 5s countdown 🙂

---
+++ Simple 5 seconds countdown.py
@@ -0,0 +1,25 @@
+# Simple Countdown by 9e1r3
+#
+import time
+#
+print("Countdown has started!")
+time.sleep(1)
+#
+print("5")
+time.sleep(1)
+#
+print("4")
+time.sleep(1)
+#
+print("3")
+time.sleep(1)
+#
+print("2")
+time.sleep(1)
+#
+print("1")
+time.sleep(1)
+#
+print("Done!")
+time.sleep(1)
+#
